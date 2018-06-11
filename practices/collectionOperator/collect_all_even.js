884fd9903997067adda327784f00574a06d091b3
yuanjiajuan <834751824@qq.com>
2018-06-11 15:26:05 +0800

    集合运算

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -6,13 +6,9 @@ function collect_all_even(collection) {
   for(var i=0;i<collection.length;i++){
     if(collection[i]%2===0){
       collection_b.push(collection[i])
-      console.log(collection[i])
-    }
+      }
   }
   return collection_b;
 }
-
-var collection_a = [1, 2, 3, 4, 5];
-collect_all_even(collection_a)
 
 module.exports = collect_all_even;
