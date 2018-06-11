62901107d675337c7038f5a608d73406c958dc52
yuanjiajuan <834751824@qq.com>
2018-06-11 14:52:35 +0800

    第一个

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -2,6 +2,17 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
+  var collection_b=[];
+  for(var i=0;i<collection.length;i++){
+    if(collection[i]%2===0){
+      collection_b.push(collection[i])
+      console.log(collection[i])
+    }
+  }
+  return collection_b;
 }
+
+var collection_a = [1, 2, 3, 4, 5];
+collect_all_even(collection_a)
 
 module.exports = collect_all_even;
--- specs/collectionOperator/collect_all_even_spec.js
+++ specs/collectionOperator/collect_all_even_spec.js
@@ -7,7 +7,7 @@ describe('collect_all_even', function () {
   var collection_a = [1, 2, 3, 4, 5];
   var collection_b = [2, 4];
 
-  it('选出给定区间中所有的偶数', function() {
+  iit('选出给定区间中所有的偶数', function() {
 
     var result = collect_all_even(collection_a);
     expect(result).toEqual(collection_b);
